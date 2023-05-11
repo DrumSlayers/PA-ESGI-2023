e9e546a4e49aefd425bdd5d0c632576073c1c4d8
skeudy <alexandre.heleta@gmail.com>
2023-05-11 14:33:35 +0200

    Ajout Multicloud

--- providers.tf
+++ providers.tf
@@ -3,6 +3,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.65.0"
+      
+    }
+    scaleway = {
+      source = "scaleway/scaleway"
+      version = "2.18.0"
     }
   }
 }
@@ -13,4 +18,12 @@ provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
   token      = var.aws_session_token
+}
+
+provider "scaleway" {
+  # Configuration du provider scaleway et authentification
+  access_key = var.scaleway_access_key
+  secret_key = var.scaleway_secret_key
+  organization_id = var.scaleway_organization_id
+  region = var.scaleway_region
 }
