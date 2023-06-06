3539a471ad71f7383efb1474876ea03dd6982c9d
skeudy <alexandre.heleta@gmail.com>
2023-06-06 15:38:20 +0200

    Installation docker

--- modules/deploy-ec2/main.tf
+++ modules/deploy-ec2/main.tf
@@ -157,8 +157,16 @@ echo $(whoami)
 echo '${join("\n", var.ssh_public_keys)}' >> /home/ubuntu/.ssh/authorized_keys
 sudo apt-get update -y
 sudo apt-get upgrade -y
-
-su - ubuntu
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 EOF
   tags = {
