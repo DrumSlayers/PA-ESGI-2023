79c2d3338d1cb4a73f1a8438d12161a6cd86e5ed
Pierre Sarret <drumslayer@drumslayer.ovh>
2023-07-06 12:04:07 +0200

    Test soutenance

--- modules/deploy-ec2/output.tf
+++ modules/deploy-ec2/output.tf
@@ -8,7 +8,7 @@ output "vm-dns" {
 output "ec2_instance_ids" {
   value = { for key, instance in aws_instance.vm : key => instance.id }
 }
-# endpoint RDC MySQL
+# endpoint RDC MySQL edit 1
 output "db_endpoint" {
   value       = aws_db_instance.mysql.endpoint
   description = "The connection endpoint for the RDS instance"
